872b9b21bbd1432699b899a44317ebd78ce03774
Tabish <kjohny030@gmail.com>
2025-08-20 01:32:16 +0500

    Time: 0 ms (100.00%) | Memory: 12.6 MB (64.46%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.py
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.py
@@ -4,11 +4,4 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        a= re.findall(r"\b\w+\b", s)
-        ret=""
-        for x in a[:0:-1]:
-            ret=ret.__add__(x)
-            ret=ret.__add__(" ")
-        ret=ret.__add__(a[0])
-        
-        return ret
+        return " ".join(s.split()[::-1])
