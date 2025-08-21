53f5073fc138dad02e56fe40ba2c23e639edacb5
Tabish <kjohny030@gmail.com>
2025-08-21 21:51:20 +0500

    Time: 25 ms (34.98%) | Memory: 12.5 MB (97.00%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,10 @@
+from collections import Counter
+class Solution:
+    def isAnagram(self, s, t):
+        if len(s)!=len(t):
+            return False
+        c1=Counter(s)
+        c2=Counter(t)
+        if c1==c2:
+            return True
+        return False
