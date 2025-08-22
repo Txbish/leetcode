dbee2c4007ca31dfecc816ca9c2a26ccf3dd8331
Tabish <kjohny030@gmail.com>
2025-08-23 00:39:40 +0500

    Time: 45 ms (36.23%) | Memory: 53.1 MB (79.28%) - LeetSync

---
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -0,0 +1,22 @@
+/**
+ * @param {any} val
+ * @return {Object}
+ */
+var expect = function(val) {
+  return {
+    toBe: function(n) {
+      if (val !== n) throw new Error("Not Equal");
+      return true;
+    },
+    notToBe: function(n) {
+      if (val === n) throw new Error("Equal");
+      return true;
+    }
+  };
+};
+
+/**
+ * Example usage:
+ * expect(5).toBe(5);      // ✅ true
+ * expect(5).notToBe(5);   // ❌ throws "Equal"
+ */
