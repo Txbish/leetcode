5c30c3a736d551bb89c2ed32b2b516ba99a54183
Tabish <kjohny030@gmail.com>
2025-08-24 19:20:15 +0500

    Time: 44 ms (55.25%) | Memory: 55.1 MB (49.48%) - LeetSync

--- 2761-array-reduce-transformation/array-reduce-transformation.js
+++ 2761-array-reduce-transformation/array-reduce-transformation.js
@@ -5,9 +5,12 @@
  * @return {number}
  */
 var reduce = function(nums, fn, init) {
-    let val = init;   // start with init
-    for (let i = 0; i < nums.length; i++) {
-        val = fn(val, nums[i]); // accumulate
+    if (nums.length===0)
+        return init
+    let val=fn(init,nums[0])
+    for(let i=1;i<nums.length;i++)
+    {
+        val=fn(val,nums[i])
     }
-    return val;
+    return val
 };
