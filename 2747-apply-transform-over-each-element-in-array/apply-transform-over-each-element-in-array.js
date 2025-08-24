28fff29e9589f5dc0734b96adcaf367639dd967a
Tabish <kjohny030@gmail.com>
2025-08-24 19:08:59 +0500

    Time: 49 ms (14.80%) | Memory: 54 MB (16.57%) - LeetSync

--- 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.js
+++ 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.js
@@ -6,7 +6,7 @@
 var map = function (arr, fn) {
   const ret = Array(arr.length);
   for (let i = 0; i < arr.length; i++) {
-    ret[i] = fn(arr[i], i, arr); // pass element, index, and array
+    ret[i] = fn(arr[i], i); // pass element, index, and array
   }
   return ret;
 };
