0a139ba068e8447bb35f0f7b58b214c034753f5e
Tabish <kjohny030@gmail.com>
2025-08-26 01:33:35 +0500

    Time: 591 ms (65.55%) | Memory: 25.4 MB (49.54%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -3,40 +3,33 @@ from typing import List
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
         
-        def mergeSort(nums, temp, l, r):
-            if l >= r:
-                return
+        def mergeSort(arr, l, r):
+            if l == r:
+                return [arr[l]]   # base case: single element
             
             mid = (l + r) // 2
-            mergeSort(nums, temp, l, mid)
-            mergeSort(nums, temp, mid + 1, r)
-            merge(nums, temp, l, mid, r)
+            left = mergeSort(arr, l, mid)
+            right = mergeSort(arr, mid + 1, r)
 
-        def merge(nums, temp, l, mid, r):
-            # copy the range into temp
-            for i in range(l, r + 1):
-                temp[i] = nums[i]
-
-            i, j = l, mid + 1
-            k = l
-
-            while i <= mid and j <= r:
-                if temp[i] <= temp[j]:
-                    nums[k] = temp[i]
-                    i += 1
+            lc = 0
+            rc = 0
+            arrs = []
+            while lc < len(left) and rc < len(right):
+                if left[lc] < right[rc]:
+                    arrs.append(left[lc])
+                    lc += 1
                 else:
-                    nums[k] = temp[j]
-                    j += 1
-                k += 1
+                    arrs.append(right[rc])
+                    rc += 1
 
-            while i <= mid:
-                nums[k] = temp[i]
-                i += 1
-                k += 1
+            while lc < len(left):
+                arrs.append(left[lc])
+                lc += 1
 
-            # No need for "while j <= r": right half is already in place
+            while rc < len(right):
+                arrs.append(right[rc])
+                rc += 1
 
-        n = len(nums)
-        temp = [0] * n  # one reusable buffer
-        mergeSort(nums, temp, 0, n - 1)
-        return nums
+            return arrs
+
+        return mergeSort(nums, 0, len(nums) - 1)
