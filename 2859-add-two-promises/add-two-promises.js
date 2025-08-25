c3c0199ffe993912111c7a7968e436e24e2bcd33
Tabish <kjohny030@gmail.com>
2025-08-26 01:50:21 +0500

    Time: 54 ms (45.27%) | Memory: 53.8 MB (70.80%) - LeetSync

--- 2859-add-two-promises/add-two-promises.js
+++ 2859-add-two-promises/add-two-promises.js
@@ -1,16 +1,4 @@
-/**
- * @param {Promise} promise1
- * @param {Promise} promise2
- * @return {Promise}
- */
 var addTwoPromises = async function(promise1, promise2) {
-    const n1=await promise1;
-    const n2=await promise2;
-
-    return new Promise(resolve=>resolve(n1+n2))
+    const [n1, n2] = await Promise.all([promise1, promise2]);
+    return n1 + n2;
 };
-
-/**
- * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
- *   .then(console.log); // 4
- */
