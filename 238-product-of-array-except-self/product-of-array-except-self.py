8699b0ad8bcbcbd8ca01b8eec6697f61ee11b47e
Tabish <kjohny030@gmail.com>
2025-12-31 22:33:06 +0500

    Time: 17 ms (91.95%) | Memory: 23.4 MB (59.26%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,22 +1,13 @@
-class Solution(object):
-    def productExceptSelf(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        n = len(nums)
-        arr = [1] * n
-        
-        # left pass
-        left = 1
-        for i in range(n):
-            arr[i] = left
-            left *= nums[i]
-        
-        # right pass
-        right = 1
-        for i in range(n - 1, -1, -1):
-            arr[i] *= right
-            right *= nums[i]
-        
-        return arr
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * (len(nums))
+
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
